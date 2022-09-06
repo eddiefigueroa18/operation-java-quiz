d39c4e74ccb2c1a5e38924b7c54ac8bc0ca48570
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-06 12:07:43 -0700

    added to JS

--- script.js
+++ script.js
@@ -1,22 +1,75 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
+const questionElement =  document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
-
-
-
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startgame);
+
+
 
 function startgame() {
     console.log("Started Game");
     startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerElement.classList.remove("hide");
+    setNextQuestion();
 }
 
-function nextQuestion () {
+
+
+function setNextQuestion () {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+        answerButtonsElement.appendChild("button");
+    })
+}
+
+
+
+
+function resetState() {
+    nextButton.classList.add("hide");
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild;
+        (answerButtonsElement.firstChild);
+    }
+}
+
+
+
+function selectAnswer(e) {
 
 }
 
-function selectAnswer () {
 
-}
+
+
+const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "<script>", correct: true},
+            {text: "<Scripting>", correct: false},
+            {text: "<javascript>", correct: false},
+            {text: "<js>", correct: false},
+        ]
+    }
+]
